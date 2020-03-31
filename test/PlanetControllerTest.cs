d4b1da4f51642b0affecd2d1cbbbce1e7799a9ba
Alex <alex@localhost.localdomain>
2020-03-31 08:33:07 +0100

    Add test

---
+++ test/PlanetControllerTest.cs
@@ -0,0 +1,17 @@
+using Xunit;  
+
+namespace testApi.test
+{
+     public class PlanetControllerTest{
+
+         [Fact]
+         public void TestName()
+         {
+         //Given
+         
+         //When
+         
+         //Then
+         }
+     }
+}
